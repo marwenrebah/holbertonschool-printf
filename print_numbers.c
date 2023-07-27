662c8e5dac63c88753423b589bb47670260807d2
marwenrebah <6863@holbertonstudents.com>
2023-07-26 23:36:47 -1200

    Printf

---
+++ print_numbers.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * print_decimal - prints a decimal
+ * @Project: arg
+ * Return: count.
+*/
+int print_decimal(va_list Project)
+{
+unsigned int name, dec, number, account;
+int d;
+account = 0;
+d = va_arg(Project, int);
+if (d < 0)
+{
+name = (d * -1);
+account += _putchar('-');
+}
+else
+name = d;
+dec = name;
+number = 1;
+while (dec > 9)
+{
+dec /= 10;
+number *= 10;
+}
+while (number >= 1)
+{
+account += _putchar(((name / number) % 10) + '0');
+number /= 10;
+}
+return (account);
+}
+/**
+ * print_integer - prints integer
+ * @Project: args
+ * Return: i
+*/
+int print_integer(va_list Project)
+{
+return (print_decimal(Project));
+}
