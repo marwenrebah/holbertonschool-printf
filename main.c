3b6a903d0810127f0ffc3ac7485dd0cc42931fc5
marwenrebah <6863@holbertonstudents.com>
2023-07-27 22:04:40 -1200

    Printf

--- main.c
+++ main.c
@@ -9,16 +9,18 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
-
+    char __attribute__ ((unused)) *s = "hello";
+    char __attribute__ ((unused)) k = 'a';
+    int __attribute__((unused)) len;
+    int __attribute__((unused)) len2;
+    unsigned int __attribute__((unused)) ui;
+    void __attribute__((unused)) *addr;
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("hello %s, %c\n", s);
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
     printf("Negative:[%d]\n", -762534);
@@ -36,9 +38,13 @@ int main(void)
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
     len2 = printf("Percent:[%%]\n");
-    _printf("Len:[%d]\n", len);
+    _printf("Len:[%d]\n", len2);
     printf("Len:[%d]\n", len2);
+    printf("Len:[%d]\n", len);
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
+    _printf("%%\n");
+    _printf("%\n");
+     _printf("%");
     return (0);
 }
