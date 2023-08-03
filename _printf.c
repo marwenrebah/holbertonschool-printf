8bc1787a2740c0499ccd2072f4c992bd7c68f174
marwenrebah <6863@holbertonstudents.com>
2023-08-03 00:20:31 -1200

    Printf

--- _printf.c
+++ _printf.c
@@ -1,42 +1,48 @@
 #include "main.h"
-
 /**
-* _printf - outputs a string
-* @format: given format
-* Return: integer
-*/
-
+ * _printf - outputs a formatted string.
+ * @format: input string containg various arguments.
+ *
+ * Return: interger
+ */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int (*print_func)(va_list);
-	int count = 0;
-	const char *p;
-
-	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p != '\0'; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			print_func = select_func(*p);
-			if (print_func)
-				count += print_func(args);
-			else
-				count += _printf("%%%c", *p);
-		}
-		else
-			count += _putchar(*p);
-	}
-	va_end(args);
-	return (count);
+int i = 0;
+va_list args;
+int (*function)(va_list) = NULL;
+if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+return (-1);
+va_start(args, format);
+while (*format)
+{
+if (*format == '%' && *(format + 1) != '%')
+{
+format++;
+function = select_func(format);
+if (*(format) == '\0')
+return (-1);
+else if (function == NULL)
+{
+_putchar(*(format - 1));
+_putchar(*format);
+i += 2;
+}
+else
+i += function(args);
+}
+else if (*format == '%' && *(format + 1) == '%')
+{
+format++;
+_putchar('%');
+i++;
+}
+else
+{
+_putchar(*format);
+i++;
+}
+format++;
+}
+va_end(args);
+return (i);
 }
